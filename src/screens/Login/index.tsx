e667c024f9ad8952e0a3b898ad29272101511207
marquisalexander <marquis.alexander@outlook.com>
2025-11-10 11:54:38 -0300

    feature: add logo tipo

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -4,11 +4,13 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { Text, TouchableOpacity, View, TextInput } from "react-native";
 import { LoginForm } from "./LoginForm";
+import { AuthHeader } from "@/components/AuthHeader";
 
 export default function Login() {
   return (
     <DismissKeyboardView>
       <View className="flex-1 w-[82%] self-center">
+        <AuthHeader />
         <LoginForm />
       </View>
     </DismissKeyboardView>
