6ed97dd35b36f2d2e0e5a37fd40644938f072aaf
marquisalexander <marquis.alexander@outlook.com>
2025-11-10 11:18:13 -0300

    feature: added form, component input

---
+++ src/components/AppInput/index.tsx
@@ -0,0 +1,81 @@
+import { colors } from "@/shared/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import clsx from "clsx";
+import { useRef, useState } from "react";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import {
+  Text,
+  TextInput,
+  TextInputProps,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+interface AppInputParams<T extends FieldValues> extends TextInputProps {
+  control: Control<T>;
+  name: Path<T>;
+  leftIconName?: keyof typeof MaterialIcons.glyphMap;
+  label?: string;
+}
+
+export const AppInput = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  leftIconName,
+  ...rest
+}: AppInputParams<T>) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const inputRef = useRef<TextInput>(null);
+
+  const checkFocus = () => {
+    if (inputRef.current) {
+      setIsFocused(inputRef.current?.isFocused);
+    }
+  };
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange, value } }) => {
+        return (
+          <View className="w-full mt-4">
+            {label && (
+              <Text
+                className={clsx(
+                  "mb-2 mt-3 text-base",
+                  isFocused ? "text-accent-brand" : "text-gray-600",
+                )}
+              >
+                {label}
+              </Text>
+            )}
+            <TouchableOpacity className="flex-row items-center justify-between border-b-[1px] border-gray-600 px-3 py-2 h-16">
+              {leftIconName && (
+                <MaterialIcons
+                  name={leftIconName}
+                  color={
+                    isFocused ? colors["accent-brand"] : colors.gray["600"]
+                  }
+                  size={24}
+                  className="mr-2"
+                />
+              )}
+              <TextInput
+                value={value}
+                {...rest}
+                onChangeText={onChange}
+                className="flex-1 text-base text-gray-500"
+                onFocus={checkFocus}
+                onEndEditing={checkFocus}
+                ref={inputRef}
+                placeholderTextColor={colors.gray["700"]}
+              />
+            </TouchableOpacity>
+          </View>
+        );
+      }}
+    />
+  );
+};
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -3,18 +3,14 @@ import { PublicStackParamsList } from "@/routes/PublicRoutes";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { Text, TouchableOpacity, View, TextInput } from "react-native";
+import { LoginForm } from "./LoginForm";
 
 export default function Login() {
-  const navigation =
-    useNavigation<StackNavigationProp<PublicStackParamsList>>();
-
   return (
     <DismissKeyboardView>
-      <Text className="text-xl font-bold text-blue-500">tela de login</Text>
-      <TextInput className="bg-gray500 w-full" />
-      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-        <Text>registrar</Text>
-      </TouchableOpacity>
+      <View className="flex-1 w-[82%] self-center">
+        <LoginForm />
+      </View>
     </DismissKeyboardView>
   );
 }
