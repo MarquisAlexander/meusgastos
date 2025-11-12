24c4d97116369b16c2a4cc7788ab995628f7ee88
marquisalexander <marquis.alexander@outlook.com>
2025-11-12 13:56:10 -0300

    feature: autentication context configuration

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,12 +1,12 @@
 import { DismissKeyboardView } from "@/components/DismissKeyboardView";
-import { PublicStackParamsList } from "@/routes/PublicRoutes";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { Text, TouchableOpacity, View, TextInput } from "react-native";
+import { View } from "react-native";
 import { LoginForm } from "./LoginForm";
 import { AuthHeader } from "@/components/AuthHeader";
+import { useAuthContext } from "@/context/auth.context";
 
 export default function Login() {
+  const { user } = useAuthContext();
+
   return (
     <DismissKeyboardView>
       <View className="flex-1 w-[82%] self-center">
