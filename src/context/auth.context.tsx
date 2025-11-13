6d09fc06da0315f62cf1f684059d00b0fc1c686c
marquisalexander <marquis.alexander@outlook.com>
2025-11-13 14:29:11 -0300

    feature: persisting user data

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -10,6 +10,8 @@ import {
 
 import * as authService from "@/shared/services/dt-money/auth.service";
 import { IUser } from "@/shared/interfaces/https/user-interface";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { IAuthenticateResponse } from "@/shared/interfaces/https/authenticate-repsonse";
 
 type AuthContextType = {
   user: IUser | null;
@@ -17,6 +19,7 @@ type AuthContextType = {
   handleAuthenticate: (params: FormLoginParams) => Promise<void>;
   handleRegister: (params: FormRegisterParams) => Promise<void>;
   handleLogout: () => void;
+  restoreUserSession: () => Promise<string | null>;
 };
 
 export const AuthContext = createContext<AuthContextType>(
@@ -30,16 +33,37 @@ export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
   const handleAuthenticate = async (userData: FormLoginParams) => {
     const { token, user } = await authService.authenticate(userData);
 
+    await AsyncStorage.setItem(
+      "dt-money-user",
+      JSON.stringify({ user, token }),
+    );
     setUser(user);
     setToken(token);
   };
+
   const handleRegister = async (formData: FormRegisterParams) => {
     const { token, user } = await authService.registerUser(formData);
 
+    await AsyncStorage.setItem(
+      "dt-money-user",
+      JSON.stringify({ user, token }),
+    );
     setUser(user);
     setToken(token);
   };
+
   const handleLogout = async () => {};
+
+  const restoreUserSession = async () => {
+    const userData = await AsyncStorage.getItem("dt-money-user");
+
+    if (userData) {
+      const { token, user } = JSON.parse(userData) as IAuthenticateResponse;
+      setUser(user);
+      setToken(token);
+    }
+    return userData;
+  };
 
   return (
     <AuthContext.Provider
