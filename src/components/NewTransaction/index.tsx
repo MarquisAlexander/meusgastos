6e8ebcf70d909aca9f6574a4e4061e5336dbc510
marquisalexander <marquis.alexander@outlook.com>
2025-11-26 17:06:00 -0300

    feature: create buttons to description and value

--- src/components/NewTransaction/index.tsx
+++ src/components/NewTransaction/index.tsx
@@ -1,9 +1,11 @@
 import { CreateTransactionInterface } from "@/shared/interfaces/https/create-transaction-request";
 import { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/shared/colors";
 import { useBottomSheetContext } from "@/context/bottomSheet.context";
+import CurrencyInput from "react-native-currency-input";
+
 export const NewTransaction = () => {
   const { closeBottomSheet } = useBottomSheetContext();
 
@@ -14,6 +16,13 @@ export const NewTransaction = () => {
     value: 0,
   });
 
+  const setTransactionData = (
+    key: keyof CreateTransactionInterface,
+    value: string | number,
+  ) => {
+    setTransaction((prevData) => ({ ...prevData, [key]: value }));
+  };
+
   return (
     <View className="px-8 py-5">
       <TouchableOpacity
@@ -23,6 +32,25 @@ export const NewTransaction = () => {
         <Text className="text-white text-xl font-bold">Nova transação</Text>
         <MaterialIcons name="close" color={colors.gray["700"]} size={20} />
       </TouchableOpacity>
+      <View className="flex-1 mt-8 mb-8">
+        <TextInput
+          value={transaction.description}
+          onChangeText={(text) => setTransactionData("description", text)}
+          placeholder="Descrição"
+          placeholderTextColor={colors.gray["700"]}
+          className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-[6] pl-4"
+        />
+        <CurrencyInput
+          value={transaction.value}
+          prefix="R$ "
+          delimiter="."
+          separator=","
+          precision={2}
+          minValue={0}
+          onChangeValue={(value) => setTransactionData("value", value ?? 0)}
+          className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-[6] pl-4"
+        />
+      </View>
     </View>
   );
 };
